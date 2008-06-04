048e0572bd70791154289a609ec1d1acf495ae1d
David Lowenfels <david@internautdesign.com>
2008-06-04 12:19:49 -0700

    changing extention of README to .rdoc

--- test/factory_worker_test.rb
+++ test/factory_worker_test.rb
@@ -1,14 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + "/_helper")
 
 class FactoryWorkerTest < Test::Unit::TestCase
-  # factory :monkey, {
-  #   :name => "George"
-  # }
-  # 
-  # factory :pirate, {
-  #   :catchphrase => "Ahhrrrr, Matey!",
-  #   :monkey      => lambda{ create_monkey }  
-  # }
 
   # TODO!
   def test_truth
