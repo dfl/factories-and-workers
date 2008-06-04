bba2a5df91cf94d629dde7585d3352fc23eedd1d
David Lowenfels <david@internautdesign.com>
2008-06-03 23:28:33 -0700

    fixing lambda problem cause by refactoring

--- lib/factory_builder.rb
+++ lib/factory_builder.rb
@@ -4,44 +4,41 @@ module Factory; end
 class Object
   include Factory
 
-  def factory( factory, default_attributes={} )
-    FactoryBuilder.new( factory, default_attributes )
+  def factory( factory, default_attrs={} )
+    FactoryBuilder.new( factory, default_attrs )
   end
 
 end
 
 class FactoryBuilder
-  def initialize( factory, default_attributes={} )
-    case factory
-    when Symbol, String
-      factory = factory.to_s
-    else
-      raise "I don't know how to build '#{factory.inspect}'"
-    end
+  def initialize( factory, default_attrs={} )
+    raise "I don't know how to build '#{factory.inspect}'" unless [ Symbol, String ].include?( factory.class )
 
     # make the valid attributes method      
-    Factory.send :define_method, :"valid_#{factory}_attributes" do
-      returning default_attributes.dup do |evaluated_attributes|
-        default_attributes.each_pair do |key, value|
-          evaluated_attributes[key] = value.call if value.is_a?(Proc)
+    valid_attrs_method = :"valid_#{factory}_attributes"
+
+    Factory.send :define_method, valid_attrs_method do
+      returning default_attrs.dup do |evaluated_attrs|
+        default_attrs.each_pair do |key, value|
+          evaluated_attrs[key] = value.call if value.is_a?(Proc)
         end
       end
     end
 
+    ar_klass = ActiveRecord.const_get( factory.to_s.classify )
+
     # make the create method
     Factory.send :define_method, :"create_#{factory}" do |*args|
-      ar_klass = ActiveRecord.const_get( factory.to_s.classify )
-      valid_attrs = self.send(:"valid_#{factory}_attributes")
-      returning obj = ar_klass.create!( valid_attrs.merge( args.first || {} ) ) do  # default to empty hash if no args provided
+      overridden_attrs = args.first || {} # default to empty hash if no args provided
+      returning ar_klass.create!( self.send( valid_attrs_method ).merge( overridden_attrs ) ) do |obj|  # default to empty hash if no args provided
         yield obj if block_given?  # magic pen
       end
     end
 
     # make the build method
     Factory.send :define_method, :"build_#{factory}" do |*args|
-      ar_klass = ActiveRecord.const_get( factory.to_s.classify )
-      valid_attrs = self.send(:"valid_#{factory}_attributes")
-      returning obj = ar_klass.new( valid_attrs.merge( args.first || {} ) ) do  # default to empty hash if no args provided
+      overridden_attrs = args.first || {} # default to empty hash if no args provided
+      returning ar_klass.new( self.send( valid_attrs_method ).merge( overridden_attrs) ) do |obj|
         yield obj if block_given?  # magic pen
       end
     end
