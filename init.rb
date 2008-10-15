6b83a9c74bc3a57e1357d3243ae1af6e2c3e4e06
David Lowenfels <david@internautdesign.com>
2008-10-14 19:48:03 -0700

    loading plugin in both test and development environments

--- init.rb
+++ init.rb
@@ -1,2 +1,24 @@
-require "factories-and-workers"
-require File.dirname(__FILE__)+"/rails/init.rb" if defined?(RAILS_ENV)
+if defined?(RAILS_ENV) && __FILE__ =~ %r{vendor/plugins} # are we running as a rails plugin?
+
+# Modify the variable below to specify in which environments this plugin should be loaded.
+  load_plugin_in = {
+    'test'        => true,
+    'development' => true,   # NOTE: often comes in handy in the console
+    'production'  => false,
+  }
+
+  if load_plugin_in[ RAILS_ENV ]
+    require "factories-and-workers"
+    require File.dirname(__FILE__)+"/rails/init.rb"
+  end
+
+  # NOTE: if using as a gem with Rails and you want similar behaviour as above, do something like this in environment.rb :
+  # config.gem 'factories-and-workers', :source => 'http://gems.github.com' unless RAILS_ENV=='production'
+
+else  # bootstrap the gem
+
+  require "factories-and-workers"  
+
+end  
+
+
