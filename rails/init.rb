4b1d0047a518a0c94b5947323666ee4313f9105a
David Lowenfels <david@internautdesign.com>
2008-10-14 20:22:30 -0700

    fixing config.after_initialize stuff

--- rails/init.rb
+++ rails/init.rb
@@ -1,6 +1,8 @@
 require 'fileutils'
 
-%w(spec/factories.rb spec/factory_workers.rb test/factories.rb test/factory_workers.rb).each do |file|
-  path = File.join(Dir.pwd, file)
-  require path if File.exists?(path)
+config.after_initialize do
+  %w(spec/factories.rb spec/factory_workers.rb test/factories.rb test/factory_workers.rb).each do |file|
+    path = File.join(Dir.pwd, file)
+    require path if File.exists?(path)
+  end
 end
